78ca4473a99835da14e0ab4dbb1dafffafa61703
maryamboneh <maryamboneh@gmail.com>
2022-02-04 19:57:53 +0330

    taxi mashad

---
+++ Taxi/js/notification.js
@@ -0,0 +1,26 @@
+
+function Notify() {
+    var notif = Notification("سفر جدید", {
+        icon:"img/taxi.svg",
+        body: "سلام خانم/آقای راننده. کجاااایی زود باش برو مسافر منتظره. البته عجله نکن!"
+    })
+
+    setTimeout(function() {notif.close()}, 5000);
+}
+
+
+function showNotification() {
+    if ("notification" in window){
+        if (Notification.permission === "granted"){
+            Notify();
+        } else if (Notification.permission !== "denied"){
+            Notification.requestPermission(function(permission){
+                if (permission === "granted" ){
+                    Notify();
+                }
+            })
+        }
+    } else {
+        alert("نوتیفیکیشن در مرورگر شما پشتیبانی نمی‌شود.")
+    }
+}
---
+++ Taxi/js/script.js
@@ -0,0 +1,117 @@
+var map;
+var cars;
+var lat = 32.4279;
+var long = 53.6880;
+flag = false;
+
+async function read_data(file){
+    let x = await fetch(file);
+    let y = await x.text();
+    cars = JSON.parse(y);    
+}
+
+function displayCars(){
+    
+    read_data("https://raw.githubusercontent.com/MaryamBoneh/Web-Programming-Course/main/Taxi/js/taxi.json");
+
+    var car1 = new google.maps.LatLng(cars[0].lat, cars[0].lng);
+    var car2 = new google.maps.LatLng(cars[1].lat, cars[1].lng);
+    var car3 = new google.maps.LatLng(cars[2].lat, cars[2].lng);
+
+    var car_position_1 = new google.maps.Marker({   
+        position: car1,
+        draggable: false,
+        icon:'img/taxi.svg',
+        animation:google.maps.Animation.BOUNCE
+    });
+    var car_position_2 = new google.maps.Marker({   
+        position: car2,
+        draggable: false,
+        icon:'img/taxi.svg',
+        animation:google.maps.Animation.BOUNCE
+    });
+    var car_position_3 = new google.maps.Marker({   
+        position: car3,
+        draggable: false,
+        icon:'img/taxi.svg',
+        animation:google.maps.Animation.BOUNCE
+    });
+    map.setCenter(car_position_1);
+    map.setCenter(car_position_2);
+    map.setCenter(car_position_3);
+}
+
+
+function getDistance(start, end){
+    x1 = start.lat()
+    y1 = start.lng()
+    x2 = end.lat()
+    y2 = end.lng()
+    var distance = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    return distance;
+}
+
+function myMap() {
+    var mapProp= {
+        center: new google.maps.LatLng(lat, long),
+        zoom: 10,
+    };
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+if (navigator.geolocation){
+    displayCars()
+    navigator.geolocation.getCurrentPosition(function(position){
+        lat = position.coords.latitude
+        long = position.coords.longitude
+
+        var myCenter = new google.maps.LatLng(lat, long);
+        map.setCenter(myCenter);
+
+        var marker_start = new google.maps.Marker({
+                position: myCenter,
+                draggable: true,
+                animation: google.maps.Animation.BOUNCE,
+                icon: 'img/position.png'
+            });
+
+        marker_start.setMap(map)
+
+        google.maps.event.addListener(marker_start, 'click', function() {
+
+            var location_start = marker_start.getPosition();
+
+            if (!flag){
+                flag = true;    
+            
+                document.getElementById("message").innerHTML = "مقصد را انتخاب کنید."
+            
+                var marker_end = new google.maps.Marker({
+                    position: myCenter,
+                    draggable: true,
+                    animation: google.maps.Animation.BOUNCE,
+                    icon: 'img/destination.png'
+                })
+            
+                marker_end.setMap(map)
+            
+                google.maps.event.addListener(marker_end, 'click', function(){
+            
+                    var location_end = marker_end.getPosition();
+                    
+                    distance = getDistance(location_start, location_end);
+                    var price = Math.round(distance * 2000);
+                    console.log(price)
+            
+                    var message = document.getElementById("message")
+                    message.innerHTML = "درخواست سفر شما ثبت شد. هزینه سفر " + price + " هزار تومان است.";
+                    message.classList.remove("alert-warning");
+                    message.classList.add("alert-success")
+                    showNotification()
+                });
+            }
+        });
+    });
+} else {
+    alert("geolocation isn't supported by this browser");
+}
