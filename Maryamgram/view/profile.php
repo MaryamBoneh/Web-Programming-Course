7a5cde21b884077c38f2da6064ea63590f6d701b
maryamboneh <maryamboneh@gmail.com>
2022-05-16 22:18:45 +0430

    add profile

---
+++ Maryamgram/view/profile.php
@@ -0,0 +1,39 @@
+<?php include "header.php"; ?>
+    <div class="profile_body mt-5">
+        <div class="container d-flex justify-content-start align-items-center">
+            <div class="profile_card card">
+                <div class="profile_upper">
+                    <img src="https://i.imgur.com/Qtrsrk5.jpg" class="img-fluid">
+                </div>
+
+                <div class="profile_user text-center">
+                    <div class="profile_profile">
+                    <img src="assets/img/users/me.png" class="rounded-circle" width="80">
+                    </div>
+                </div>
+
+                <div class="text-center profile_content">
+                    <h4 class="mb-0">Maryam Boneh</h4>
+                    <span class="text-muted d-block mb-2">I'm UI Designer and Programmer</span>
+                    <!-- <button class="btn btn-primary btn-sm follow">Follow</button> -->
+                    <div class="d-flex justify-content-between align-items-center mt-4 px-4">
+                        <div class="profile_stats">
+                            <h6 class="mb-0">Followers</h6>
+                            <span>223</span>
+                        </div>
+
+                        <div class="profile_stats">
+                            <h6 class="mb-0">Following</h6>
+                            <span>185</span>
+                        </div>
+                        
+                        <div class="profile_stats">
+                            <h6 class="mb-0">Posts</h6>
+                            <span>85</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include "footer.php"; ?> 
